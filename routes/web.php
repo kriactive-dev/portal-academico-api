f5f3b80e57afe295ea9479e9877f440e89d229cc
Kelven Bragoa <kelvenbragoa@hotmail.com>
2026-02-12 19:46:01 +0200

    feat: change welcome message

--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,9 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return response()->json([ 'message' => 'Welcome to the API 1.0',
+                                'version' => '1.0', 
+                                'status' => 'success'
+    
+    ]);
 });
