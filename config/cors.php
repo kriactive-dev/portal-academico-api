6df77076d3ab3c5aa7ef730748c003cbec8df11e
Kelven Bragoa <kelvenbragoa@hotmail.com>
2026-02-16 19:09:17 +0200

    feat:  cors configuration

--- config/cors.php
+++ config/cors.php
@@ -19,7 +19,10 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['*'],
+    'allowed_origins' => [
+        'https://chatbot.ucm.ac.mz',
+        'http://chatbot.ucm.ac.mz',  // Caso alguém acesse sem HTTPS
+    ],
 
     'allowed_origins_patterns' => [],
 
@@ -29,6 +32,6 @@ return [
 
     'max_age' => 0,
 
-    'supports_credentials' => false,
+    'supports_credentials' => true,
 
 ];
